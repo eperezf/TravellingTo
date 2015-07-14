0111bc2970feba4d1406fcd35ddb5e1c794a1c51
Eduardo Pérez <eperezf@pisapapeles.net>
2015-07-14 00:44:02 -0300

    added cdn to interface.php

---
+++ interface.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		WOHOO!
+	</div>
+</body>
+</html>
