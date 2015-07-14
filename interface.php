d6c29729e3d6290e28a911e1c2a1a7184df37e48
Eduardo Pérez <eperezf@pisapapeles.net>
2015-07-14 03:01:22 -0300

    Almost finished interface

--- interface.php
+++ interface.php
@@ -5,10 +5,113 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/flatly/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<div class="container">
-		WOHOO!
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header text-center">
+  				<h1><small>Travelling from</small> COUNTRYFROM <small>to</small> COUNTRYTO</h1>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<!--COUNTRY FROM GENERIC INFO-->
+			<div class="col-md-6 col-lg-6 col-sm-12">
+				<div class="panel panel-info">
+				  <div class="panel-heading">
+				    <h3 class="panel-title"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> INFORMATION OF COUNTRYFROM</h3>
+				  </div>
+				  <div class="panel-body">
+				  	<img src="http://placehold.it/320x168" class="img-responsive"></br>
+				    Here goes information relevant to this country but not comparable to the other. For example, country number code, populaton and that kind of stuff. There will be an want/need information button for contacting me regarding getting info corrected or adding more.
+				  </div>
+				</div>
+			</div>
+			<!--COUNTRY FROM GENERIC INFO END-->
+
+			<!--COUNTRY TO GENERIC INFO-->
+			<div class="col-md-6 col-lg-6 col-sm-12">
+				<div class="panel panel-info">
+				  <div class="panel-heading">
+				    <h3 class="panel-title"><span class="glyphicon glyphicon-plane" aria-hidden="true"></span> INFORMATION OF COUNTRYTO</h3>
+				  </div>
+				  <div class="panel-body">
+				  	<img src="http://placehold.it/320x168" class="img-responsive"></br>
+				    Here goes information relevant to this country but not comparable to the other. For example, country number code, populaton and that kind of stuff. There will be an want/need information button for contacting me regarding getting info corrected or adding more.
+				  </div>
+				</div>
+			</div>
+			<!--COUNTRY TO GENERIC INFO END-->
+
+			<!--COUNTRY LANGUAGE COMPARISON-->
+			<div class="col-md-12">
+				<div class="panel panel-info">
+				  <div class="panel-heading">
+				    <h3 class="panel-title"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> LANGUAGE COMPARISON BETWEEN COUNTRYFROM AND COUNTRYTO</h3>
+				  </div>
+				  <div class="panel-body">
+						This is the language comparison between COUNTRYFROM and COUNTRYTO. If both are the same, this will be OK. If not, it shows common phrases to learn. A translator should be useful here.
+					</div>
+				</div>
+			</div>
+			<!--COUNTRY LANGUAGE COMPARISON END-->
+
+			<!--COUNTRY MONEY COMPARISON-->
+			<div class="col-md-12">
+				<div class="panel panel-info">
+				  <div class="panel-heading">
+				    <h3 class="panel-title"><span class="glyphicon glyphicon-usd" aria-hidden="true"></span> CURRENCY COMPARISON BETWEEN COUNTRYFROM AND COUNTRYTO</h3>
+				  </div>
+				  <div class="panel-body">
+						In COUNTRYFROM the official currency is the MONEYFROM (MONEYCODEFROM) meanwhile in COUNTRYTO te official currency is MONEYTO (MONEYCODETO) is used. </br>
+						If needed, there will be a currency comparison system. </br>
+						If not, it will be ok to go!
+					</div>
+				</div>
+			</div>
+			<!--COUNTRY MONEY COMPARISON END-->
+
+			<!--COUNTRY VOLTAGE COMPARISON-->
+			<div class="col-md-12">
+				<div class="panel panel-info">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">VOLTAGE COMPARISON BETWEEN COUNTRYFROM AND COUNTRYTO</h3>
+				  </div>
+				  <div class="panel-body">
+						IF both the voltage in COUNTRYFROM and COUNTRYTO are the same, there will be an OK sign an we'll be good to go. If not, there will be a breakdown of which voltages each country uses and if they are compatible. Also a warning for different plugs (WIP).
+					</div>
+				</div>
+			</div>
+			<!--COUNTRY VOLTAGE COMPARISON END-->
+
+			<!--COUNTRY PLUG COMPARISON-->
+			<div class="col-md-12">
+				<div class="panel panel-info">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">PLUG COMPARISON BETWEEN COUNTRYFROM AND COUNTRYTO</h3>
+				  </div>
+				  <div class="panel-body">
+						This is totally going into the WIP part. I don't know how the fuck to do that shit. 
+					</div>
+				</div>
+			</div>
+			<!--COUNTRY PLUG COMPARISON END-->
+
+			<!--COUNTRY ROAD COMPARISON-->
+			<div class="col-md-12">
+				<div class="panel panel-info">
+				  <div class="panel-heading">
+				    <h3 class="panel-title"><span class="glyphicon glyphicon-road" aria-hidden="true"></span> DRIVING SIDE COMPARISON BETWEEN COUNTRYFROM AND COUNTRYTO</h3>
+				  </div>
+				  <div class="panel-body">
+						If people drive on the same side, it's great! If not, advice! Still, check for special driving licenses you may need. Get that info too, motherfucker!
+					</div>
+				</div>
+			</div>
+			<!--COUNTRY ROAD COMPARISON END-->
+		</div>
 	</div>
 </body>
 </html>
