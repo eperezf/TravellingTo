10c853c100a6673d375bcdfd4ce154609ec249a0
Eduardo Pérez <eperezf@pisapapeles.net>
2015-10-06 09:00:20 -0300

    Close #3 - About page

---
+++ about.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+	<title>About Us | TravellingTo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/darkly/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		    <span class="sr-only">Toggle navigation</span>
+		    <span class="icon-bar"></span>
+		    <span class="icon-bar"></span>
+		    <span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">TravellingTo</a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+		    <li><a href="index.php">Index</a></li>
+		    <li><a href="about.php">About Us</a></li>
+		    <li><a href="disclaimer.php">Disclaimer</a></li>
+		    <li><a href="helpus.php">Contribute</a></li>
+				<li><a href="contact.php">Contact Us</a></li>
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+  			<h1>About Us</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			We are two engineer students working on a small project for our Programming course. We saw there were many websites with information about countries and its differences. But there wasn't any website that grouped all the information in one place and compard automatically. That's why we created TravellingTo. In this website you can see what will you need to travel to another country. For example, what types of plugs do they use, or the standard voltage. 
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+  			<h1>Our goal</h1>
+			</div>
+			Our goal is to create a <strong>free, useful website for everyone</strong> who travels from one country to another and needs essential information. <strong>We, like many people on the internet, hate ads.</strong> That's why we are running this website by <strong>donations only.</strong> We don't have money to pay the API access to the currency conversion or the servers so <strong>any donation is very useful so we can keep this website running and free of ads.</strong>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+  			<h1>Who we are</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+  	<div class="col-sm-6 col-md-6 col-xs-12">
+    	<div class="thumbnail">
+      	<img src="http://placehold.it/600x400" alt="...">
+      	<div class="caption">
+        	<h3>Eduardo Pérez</h3>
+        	<p>Fan of computers, videogames, electronic music and cars. Co-Founder of <a href="http://pisapapeles.net">Pisapapeles.net</a></p>
+        	<p><a href="#" class="btn btn-primary" role="button">Twitter</a> <a href="#" class="btn btn-default" role="button">Email</a></p>
+      	</div>
+    	</div>
+  	</div>
+  	<div class="col-sm-6 col-md-6 col-xs-12">
+    	<div class="thumbnail">
+      	<img src="http://placehold.it/600x400" alt="...">
+      	<div class="caption">
+        	<h3>Vicente Rosati</h3>
+        	<p>Description goes here!</p>
+        	<p><a href="#" class="btn btn-default" role="button">Email</a></p>
+      	</div>
+    	</div>
+  	</div>
+	</div>
+</div>
+</body>
+</html>
