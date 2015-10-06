f5ddfd2367be373c9670535c2aa12292296ee70e
rosati13 <vicenterosati@gmail.com>
2015-10-06 18:44:50 -0300

    Task Index/home done 100%

--- result.php
+++ result.php
@@ -108,7 +108,7 @@ session_start();
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		  <ul class="nav navbar-nav">
-		    <li><a href="index.php">Index</a></li>
+		    <li><a href="index.php">Home</a></li>
 		    <li><a href="about.php">About Us</a></li>
 		    <li><a href="disclaimer.php">Disclaimer</a></li>
 		    <li><a href="/contribute">Contribute</a></li>
