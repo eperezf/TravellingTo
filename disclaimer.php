95b9229d3981055bd4525581d1a9795ae9ac5153
Eduardo Pérez <eperezf@pisapapeles.net>
2015-10-08 14:48:16 -0300

    Added donate button to right nabber

--- disclaimer.php
+++ disclaimer.php
@@ -31,6 +31,14 @@
 		    <li><a href="/contribute">Contribute</a></li>
 				<li><a href="/contact.php">Contact Us</a></li>
 		  </ul>
+		  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="navbar-form navbar-right">
+        <div class="form-group">
+          <input type="hidden" name="cmd" value="_s-xclick">
+          <input type="hidden" name="hosted_button_id" value="SH7D727QBBHWE">
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!">
+          <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
+        </div>
+      </form>
 		</div><!-- /.navbar-collapse -->
 	</div><!-- /.container-fluid -->
 </nav>
